d7330354ee18326877b5725f49aade706bd4aa62
Kaipaev Timur <kaipaevtimur55@gmail.com>
2022-02-02 12:16:15 +0200

    added  new structure

--- GitCommit.playground/Contents.swift
+++ GitCommit.playground/Contents.swift
@@ -10,3 +10,19 @@ struct Fahtenheit {
 var temp = Fahtenheit()
 print("The temperature is now \(temp.temperature)°")
 
+struct Celsius {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+}
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+
+print(freezingPointOfWater)
+
+print(boilingPointOfWater)
